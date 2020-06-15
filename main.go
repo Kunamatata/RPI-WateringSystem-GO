c485fa2443a7770215aac1c44c1b3ee3f54e5156
Max Caly <mcaly@skyslope.com>
2020-06-14 21:02:11 -0700

    feat: add timed endpoint

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 	"watering-system-go/wateringrpio"
 
 	"github.com/go-chi/chi"
@@ -27,7 +28,13 @@ type Command struct {
 	State string
 }
 
-func TurnOnWateringSystem(backyardPin wateringrpio.PinWrapper, frontyardPin wateringrpio.PinWrapper, w http.ResponseWriter, r *http.Request) {
+type TimedCommand struct {
+	Zone          string
+	State         string
+	TimeInSeconds int64
+}
+
+func turnOnWateringSystem(backyardPin wateringrpio.PinWrapper, frontyardPin wateringrpio.PinWrapper, w http.ResponseWriter, r *http.Request) {
 	var command Command
 	json.NewDecoder(r.Body).Decode(&command)
 	if command.Zone == "frontyard" && command.State == "on" {
@@ -46,6 +53,29 @@ func TurnOnWateringSystem(backyardPin wateringrpio.PinWrapper, frontyardPin wate
 	log.Println(backyardPin.ReadPin())
 }
 
+func timedWateringSystem(backyardPin wateringrpio.PinWrapper, frontyardPin wateringrpio.PinWrapper, w http.ResponseWriter, r *http.Request) {
+	var command TimedCommand
+	json.NewDecoder(r.Body).Decode(&command)
+	log.Printf("Turning on %s\n", command.Zone)
+	if command.Zone == "frontyard" && command.State == "on" {
+		go func() {
+			frontyardPin.TurnOn()
+			time.Sleep(time.Second * time.Duration(command.TimeInSeconds))
+			frontyardPin.TurnOff()
+			log.Printf("Turning off %s", command.Zone)
+		}()
+	}
+	if command.Zone == "backyard" && command.State == "on" {
+		go func() {
+			log.Println(command.TimeInSeconds)
+			backyardPin.TurnOn()
+			time.Sleep(time.Second * time.Duration(command.TimeInSeconds))
+			backyardPin.TurnOff()
+			log.Printf("Turning off %s", command.Zone)
+		}()
+	}
+}
+
 func main() {
 	backyardPin, frontyardPin := wateringrpio.InitRPI()
 	server := initServer()
@@ -53,7 +83,10 @@ func main() {
 	log.Println(backyardPin.ReadPin())
 
 	server.router.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		TurnOnWateringSystem(backyardPin, frontyardPin, w, r)
+		turnOnWateringSystem(backyardPin, frontyardPin, w, r)
+	})
+	server.router.Post("/timed", func(w http.ResponseWriter, r *http.Request) {
+		timedWateringSystem(backyardPin, frontyardPin, w, r)
 	})
 	http.ListenAndServe(":3000", server.router)
 }
